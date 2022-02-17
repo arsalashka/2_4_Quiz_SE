8e867e0bfa5a6513044ab8a23e515ea5784fdd12
Arsalan Balzhinimaev <arsalashka@gmail.com>
2022-02-17 07:27:52 +0300

    Initial Commit

---
+++ 2_4_Quiz_SE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2_4_Quiz_SE
+//
+//  Created by Arsalan Balzhinimaev on 17.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
